4488927819ad6a94d7ec3133daf9bbf5fd811b13
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-04-08 08:28:22 +0200

    Sync latest updates from ansible core PR.

--- docker_machine.py
+++ docker_machine.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 DOCUMENTATION = '''
     name: docker_machine
     plugin_type: inventory
-    short_description: Docker machine inventory source
+    short_description: Docker Machine inventory source
     requirements:
         - L(Docker Machine,https://docs.docker.com/machine/)
     extends_documentation_fragment:
@@ -18,14 +18,14 @@ DOCUMENTATION = '''
         - Get inventory hosts from Docker Machine.
         - Uses a YAML configuration file that ends with docker_machine.(yml|yaml).
         - The plugin returns an I(all) group of nodes and one group per driver (e.g. digitalocean).
-        - The plugin sets standard host variables I(ansible_host), I(ansible_port), I(ansible_user) and I(ansible_ssh_private_key).
-        - The plugin also sets standard host variable I(ansible_ssh_common_args) to '-o StrictHostKeyChecking=no'.
+        - The plugin sets standard host variables C(ansible_host), C(ansible_port), C(ansible_user) and C(ansible_ssh_private_key).
+        - The plugin also sets standard host variable I(ansible_ssh_common_args) to C(-o StrictHostKeyChecking=no).
         - The plugin also stores the Docker Machine 'env' variables in I(dm_) prefixed host variables.
 
     options:
         plugin:
           description: token that ensures this is a source file for the C(docker_machine) plugin.
-          required: True
+          required: yes
           choices: ['docker_machine']
         verbose_output:
             description: Toggle to (not) include all available nodes metadata (e.g. Image, Region, Size) as a JSON object.
@@ -33,12 +33,10 @@ DOCUMENTATION = '''
             default: yes
         split_tags:
           description: for keyed_groups add two variables as if the tag were actually a key value pair separated by a colon, instead of just a single value.
-          required: False
           type: bool
           default: no
         split_separator:
           description: for keyed_groups when splitting tags this is the separator to split the tag value on.
-          required: False
           type: str
           default: ":"
 '''
@@ -49,18 +47,22 @@ plugin: docker_machine
 
 # Example using constructed features to create groups
 # keyed_groups may be used to create custom groups
-strict: False
+strict: no
 keyed_groups:
   - prefix: tag
     key: 'dm_tags'
 
 # Example using tag splitting where the tag is like 'dm_tag_gantry_component:routinator'
-strict: False
-split_tags: True
+strict: no
+split_tags: yes
 split_separator: ":"
 keyed_groups:
   - prefix: gantry_component
     key: 'dm_tag_gantry_component'
+
+# Example using compose to override the default SSH behaviour of asking the user to accept the remote host key
+compose:
+  ansible_ssh_common_args: '"-o StrictHostKeyChecking=accept-new"'
 '''
 
 from ansible.errors import AnsibleError
@@ -81,6 +83,44 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
     def _run_command(self, *args):
         return subprocess.check_output(["docker-machine"] + list(args)).decode('utf-8').strip()
 
+    def _set_docker_daemon_variables(self, id):
+        '''
+        Capture settings from Docker Machine that would be needed to connect to the remote Docker daemon installed on
+        the Docker Machine remote host. Note: passing '--shell=sh' is a workaround for 'Error: Unknown shell'.
+        '''
+        env_out = self._run_command('env', '--shell=sh', id)
+
+        # example output of docker-machine env --shell=sh:
+        #   export DOCKER_TLS_VERIFY="1"
+        #   export DOCKER_HOST="tcp://134.209.204.160:2376"
+        #   export DOCKER_CERT_PATH="/root/.docker/machine/machines/routinator"
+        #   export DOCKER_MACHINE_NAME="routinator"
+        #   # Run this command to configure your shell:
+        #   # eval $(docker-machine env --shell=bash routinator)
+
+        # capture any of the DOCKER_xxx variables that were output and create Ansible host vars
+        # with the asme name and value but with a dm_ name prefix.
+        for line in env_out.splitlines():
+            match = re.search('(DOCKER_[^=]+)="([^"]+)"', line)
+            if match:
+                env_var_name = match.group(1)
+                env_var_value = match.group(2)
+                self.inventory.set_variable(id, 'dm_{0}'.format(env_var_name), env_var_value)
+
+    def _set_tag_variables(self, id):
+        tags = self.node_attrs['Driver'].get('Tags') or ''
+        if tags:
+            split_tags = self.get_option('split_tags')
+            split_separator = self.get_option('split_separator')
+
+            kv_pairs = [kv_pair.strip() for kv_pair in tags.split(',') if kv_pair.strip()]
+            for kv_pair in kv_pairs:
+                if split_tags and split_separator in kv_pair:
+                    k, v = kv_pair.split(split_separator)
+                    self.inventory.set_variable(id, 'dm_tag_{0}'.format(k), v)
+                else:
+                    self.inventory.set_variable(id, 'dm_tag_{0}'.format(kv_pair))
+
     def _populate(self):
         self.inventory.add_group('all')
 
@@ -99,34 +139,11 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 self.inventory.set_variable(id, 'ansible_host', self.node_attrs['Driver']['IPAddress'])
                 self.inventory.set_variable(id, 'ansible_port', self.node_attrs['Driver']['SSHPort'])
                 self.inventory.set_variable(id, 'ansible_user', self.node_attrs['Driver']['SSHUser'])
-                self.inventory.set_variable(id, 'ansible_ssh_common_args', '-o StrictHostKeyChecking=no')
                 self.inventory.set_variable(id, 'ansible_ssh_private_key_file', self.node_attrs['Driver']['SSHKeyPath'])
 
-                # pass '--shell=bash' to workaround 'Error: Unknown shell
-                env_out = self._run_command('env', '--shell=bash', id)
+                self._set_docker_daemon_variables(id)
 
-                # example output of docker-machine env
-                #   export DOCKER_TLS_VERIFY="1"
-                #   export DOCKER_HOST="tcp://134.209.204.160:2376"
-                #   export DOCKER_CERT_PATH="/root/.docker/machine/machines/routinator"
-                #   export DOCKER_MACHINE_NAME="routinator"
-                #   # Run this command to configure your shell:
-                #   # eval $(docker-machine env --shell=bash routinator)
-
-                for env_var_name in ['DOCKER_TLS_VERIFY', 'DOCKER_HOST', 'DOCKER_CERT_PATH', 'DOCKER_MACHINE_NAME']:
-                    env_var_value = re.search('{0}="([^"]+)"'.format(env_var_name), env_out).group(1)
-                    self.inventory.set_variable(id, 'dm_{0}'.format(env_var_name), env_var_value)
-
-                # Capture any tags
-                split_tags = self.get_option('split_tags')
-                split_separator = self.get_option('split_separator')
-                tags = self.node_attrs['Driver']['Tags']
-                for kv_pair in tags.split(','):
-                    if split_tags and split_separator in kv_pair:
-                        k, v = kv_pair.split(split_separator)
-                        self.inventory.set_variable(id, 'dm_tag_{0}'.format(k), v)
-                    else:
-                        self.inventory.set_variable(id, 'dm_tag_{0}'.format(kv_pair))
+                self._set_tag_variables(id)
 
                 if self.get_option('verbose_output'):
                     self.inventory.set_variable(id, 'docker_machine_node_attributes', self.node_attrs)
@@ -144,7 +161,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 self._add_host_to_keyed_groups(self.get_option('keyed_groups'), self.node_attrs, id, strict=strict)
 
         except Exception as e:
-            raise AnsibleError('Unable to fetch hosts from Docker machine, this was the original exception: %s' %
+            raise AnsibleError('Unable to fetch hosts from Docker Machine, this was the original exception: %s' %
                                to_native(e))
 
     def verify_file(self, path):
